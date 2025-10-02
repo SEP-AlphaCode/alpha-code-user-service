14daf05fa49efe99c61954a4acb17280035b0308
mineduck1608 <ducdm160804@gmail.com>
2025-10-02 14:06:07 +0700

    Update UserService.java

--- src/main/java/com/alpha_code/alpha_code_user_service/grpc/server/UserService.java
+++ src/main/java/com/alpha_code/alpha_code_user_service/grpc/server/UserService.java
@@ -3,7 +3,9 @@ package com.alpha_code.alpha_code_user_service.grpc.server;
 import com.alpha_code.alpha_code_user_service.enums.AccountEnum;
 import com.alpha_code.alpha_code_user_service.service.AccountService;
 import io.grpc.stub.StreamObserver;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 import user.User;
@@ -14,12 +16,19 @@ import java.util.UUID;
 
 @GrpcService
 @RequiredArgsConstructor
+@Slf4j
 public class UserService extends UserServiceGrpc.UserServiceImplBase {
     private final AccountService accountService;
+
+    @PostConstruct
+    public void init() {
+        log.info("UserServiceGrpc initialized successfully");
+    }
 
     @Override
     public void getAccount(User.GetAccountRequest request, StreamObserver<User.AccountInformation> responseObserver) {
         try {
+            log.info("Received GetAccount request for accountId: {}", request.getAccountId());
             UUID accountId = UUID.fromString(request.getAccountId());
             var account = accountService.getById(accountId);
 
